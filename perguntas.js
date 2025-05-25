0a12d97fb796786e05bfd2e80c9a30a464435e8f
kamillylayza <layza.amorim@escola.pr.gov.br>
2025-05-25 18:15:04 +0000

    final

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,59 @@ criaCartao(
     'Qual o maior órgão do corpo humano?',
     'A pele é o maior órgão do corpo humano, nos adultos chega a 2m2 e cerca de 3,5 kg.'
 )
+
+criaCartao(
+    'Corpo Humano',
+    ' Qual o menor e o maior osso do corpo?',
+    'O menor osso do corpo humano é o estribo, localizado no ouvido médio.'
+)
+
+criaCartao(
+    'Corpo Humano',
+    'Qual o maior osso do corpo?',
+    'O maior é o fêmur, o osso da coxa.'
+)
+criaCartao(
+    'Corpo Humano',
+    'Qual o comprimento do intestino?',
+    'O intestino delgado pode chegar a 9 metros de comprimento. Com o intestino grosso o conjunto pode ultrapassar os 10 metros de comprimento.'
+)
+
+criaCartao(
+    'Corpo Humano',
+    'Qual a quantidade de sangue que existe no corpo?',
+    'O corpo humano possui cerca de 5 litros de sangue. Em uma doação de sangue, são cedidos no máximo 450 ml.'
+
+)
+
+criaCartao(
+    'Corpo Humano',
+    ' Quantos fios de cabelo tem na cabeça de uma pessoa?',
+    'Os folículos capilares já estão presentes nos recém-nascidos e somam ao todo 150 mil.'
+)
+
+criaCartao(
+    'Corpo Humano',
+    'Quantas vezes por dia bate um coração?',
+    'Um coração adulto bate cerca de 100 mil vezes ao dia. '
+)
+
+criaCartao(
+    'Corpo Humano',
+    'Quantas vezes por ano bate um coração?',
+    'Um coração adulto bate cerca de 100 mil vezes ao dia. Isso quer dizer que em um único ano o principal músculo do corpo terá batido 36,5 milhões de vezes.'
+)
+
+
+criaCartao(
+    'Corpo Humano',
+    'Um bebê tem mais ossos que um adulto?',
+    'O corpo de um adulto possui 206 ossos. No entanto, ainda quando recém-nascido, o ser humano apresenta certa de 300 ossos. A diferença se dá, pois, ao longo do crescimento, muitos ossos acabam se fundindo, tornando-se apenas um. '
+)
+
+criaCartao(
+    'Corpo Humano',
+    'O ser humano respira por uma narina de cada vez?',
+    'Uma coisa que poucos imaginam está relacionada a nossa respiração. Não é possível respirar pelas duas narinas ao mesmo tempo. Por isso, quando estamos resfriados ou com gripe, somente uma narina acaba entupida por vez. '
+)
+
